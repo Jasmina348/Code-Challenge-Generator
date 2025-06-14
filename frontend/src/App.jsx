4d0cfdc09674d9b6721dc7ae86a9ea6e01e05021
Jasmina348 <jasminakunwar348@gmail.com>
2025-06-14 17:08:41 -0600

    Subdirectory Created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import {  Routes, Route} from "react-router-dom";
 import { ClerkProviderWithRoutes } from "./auth/ClerkProviderWithRoutes";
 
 function App() {
 
   return <ClerkProviderWithRoutes>
-    
+<Routes>
+  
+</Routes>
   </ClerkProviderWithRoutes>
 }
 
