eab35265206dc81adfa76387e7582d55f0432b67
Jasmina348 <jasminakunwar348@gmail.com>
2025-06-15 20:31:03 -0600

    LLM added to generate the challenge

--- backend/src/routes/challenge.py
+++ backend/src/routes/challenge.py
@@ -11,6 +11,7 @@ from ..database.db import (
 
 from ..utils import authenticate_and_get_user_details
 from ..database.models import get_db
+from ..ai_generator import generate_challenge_with_ai
 import json
 from datetime import datetime
 
@@ -35,11 +36,28 @@ async def generate_challenge(request:ChallengeRequest, db:Session = Depends(get_
         if quota.remaining_quota <= 0:
             raise HTTPException(status_code=429, detail ="Quota Exhausted")
         
+        challenge_data= generate_challenge_with_ai(request.difficulty)
+        new_challenge = create_challenge(
+            db=db,
+            difficulty=request.difficulty,
+            created_by=user_id,
+            **challenge_data
+        )
+        
         challenge_data= None
     
         quota.remaining_quota -= 1
         db.commit()
-        return challenge_data
+        return {
+            "id":new_challenge.id,
+            "diffculty":request.difficulty,
+            "title": new_challenge.title,
+            "options":json.loads(new_challenge.options),
+            "correct_answer_id":new_challenge.correct_answer_id,
+            "explanation":new_challenge.explanation,
+            "timestamp":new_challenge.date_created.isoformat()
+            
+        }
     
     except Exception as e:
         raise HTTPException(status_code = 400, detail=str(e))
