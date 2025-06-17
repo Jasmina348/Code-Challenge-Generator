26258ec3953e7340d4b6339cff7f0ad0579d01fb
Jasmina348 <jasminakunwar348@gmail.com>
2025-06-17 12:33:12 -0600

    code cleanup & models added

--- frontend/src/challenge/ChallengeGenerator.jsx
+++ frontend/src/challenge/ChallengeGenerator.jsx
@@ -1,22 +1,62 @@
 import "react";
 import { useState, useEffect } from "react";
 import { MCQChallenge } from "./MCQChallenge";
+import {useApi} from '../utils/api'
+
 export function ChallengeGenerator() {
   const [challenge, setChallenge] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [difficulty, setDifficulty] = useState("easy");
   const [quota, setQuota] = useState(null);
+  const {makeRequest} =useApi()
 
-  const fetchQuota = async () => {};
-  const generateChallenge = async () => {};
-  const getNextResetTime = () => {};
+  useEffect(() => {
+        fetchQuota()
+    }, [])
+
+    const fetchQuota = async () => {
+        try {
+            const data = await makeRequest("quota")
+            setQuota(data)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const generateChallenge = async () => {
+        setIsLoading(true)
+        setError(null)
+
+        try {
+            const data = await makeRequest("generate-challenge", {
+                method: "POST",
+                body: JSON.stringify({difficulty})
+                }
+            )
+            setChallenge(data)
+            fetchQuota()
+        } catch (err) {
+            setError(err.message || "Failed to generate challenge.")
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    const getNextResetTime = () => {
+        if (!quota?.last_reset_data) return null
+        const resetDate = new Date(quota.last_reset_data)
+        resetDate.setHours(resetDate.getHours() + 24)
+        return resetDate
+    }
+
+
   return (
     <div className="challenge-container">
       <h2>Coding Challenge Generator</h2>
       <div className="quota-display">
         <p>Challenges remaining today: {quota?.quota_remaining || 0}</p>
-        {quota?.quota_remaining === 0 && <p>Next reset: {0}</p>}
+        {quota?.quota_remaining === 0 && <p>Next reset: {getNextResetTime()?.toLocaleString()}</p>}
       </div>
       <div className="difficulty-selector">
         <label htmlFor="difficulty"> Select Difficulty</label>
@@ -33,7 +73,8 @@ export function ChallengeGenerator() {
       </div>
       <button
         onClick={generateChallenge}
-        disabled={isLoading || quota?.quota_remaining === 0}
+        //disabled={isLoading || quota?.quota_remaining === 0}
+        disabled={false}
         className="generate-button"
       >
         {isLoading ? "Generation..." : "Generate Challenge"}
